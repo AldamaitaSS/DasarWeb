f4c685fefafa0f99168b0aa8a90f2a0024afee6b
AldamaitaSS <pijarheksa02@gmail.com>
2024-03-05 11:18:16 +0700

    pertemuan4 - praktikum1

---
+++ pertemuan4/variabel_konstanta.php
@@ -0,0 +1,33 @@
+<?php
+// $angka1 = 10;
+// $angka2 = 5;
+// $hasil = $angka1 + $angka2;
+// echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+
+
+// $benar = true;
+// $salah = false;
+// echo "'Variabel benar : $benar, Variabel salah : $salah"; 
+
+// Mendefinisikan konstanta untuk nilai tetap
+// define("NAMA_SITUS", "WebsiteKu.com");
+// define("TAHUN_PENDIRIAN", 2023);
+
+// echo "Selamat datang di " . NAMA_SITUS . ", situs yang di dirikan pada tahun " . TAHUN_PENDIRIAN. ".";
+
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.\n";
+
+$benar = true;
+$salah = false;
+echo "Variabel benar : $benar,\nVariabel salah : $salah\n";
+
+// Mendefinisikan konstanta untuk nilai tetap
+define("NAMA_SITUS", "WebsiteKu.com");
+define("TAHUN_PENDIRIAN", 2023);
+
+echo "Selamat datang di " . NAMA_SITUS . ",\nsitus yang di dirikan pada tahun " . TAHUN_PENDIRIAN . ".\n";
+
+?> 
