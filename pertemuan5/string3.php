fe1316e898a35438034a2625a38178ecbe97510a
AldamaitaSS <pijarheksa02@gmail.com>
2024-03-19 13:40:42 +0700

    pertemuan5 - String

---
+++ pertemuan5/string3.php
@@ -0,0 +1,13 @@
+<?php
+$pesan = "Saya arek malang";
+// echo strrev($pesan) . "<br>";
+
+#ubah variabel $pesan menjadi array dengan perintah explode
+$pesanPerkata = explode(" ", $pesan);
+#ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerkata = array_map(fn($pesan) => strrev($pesan), $pesanPerkata);
+#gabung kembali array menjadi string
+$pesan = implode(" ", $pesanPerkata);
+
+echo $pesan . "<br>";
+?>
