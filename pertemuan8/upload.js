80f3baf563f0e63ab0736a8c3359f07a40e2c511
AldamaitaSS <pijarheksa02@gmail.com>
2024-04-08 11:42:59 +0700

    pertemuan 8 - praktikum 4

--- pertemuan8/upload.js
+++ pertemuan8/upload.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+    //praktikum 4
+    $('#file').change(function() {
+        if (this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', false).css('opacity', 0.5);
+        }
+    });
+
+    //praktikum 3
     $('#upload-form').submit(function(e){
         
         var formData = new FormData(this);
